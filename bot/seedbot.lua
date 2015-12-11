fe4f7f10fffe64e50e7ff22cb37041fad64a2ecf
mamaddeveloper <mr.ashkan7786@gmail.com>
2015-12-11 22:30:37 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -219,7 +219,7 @@ function create_config( )
     "download_media",
     "invite"
     },
-    sudo_users = {94477327,tonumber(our_id)},--Sudo users
+    sudo_users = {94477327},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
